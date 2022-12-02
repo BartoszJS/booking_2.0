16a794e406ca775c6de80e4137df29b4020bcd43
BartoszJS <bartosz290300@wp.pl>
2022-12-02 14:56:35 +0100

    24 99%

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { HiHome } from 'react-icons/hi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '+100%' },
+};
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   const auth = getAuth();
@@ -22,46 +31,92 @@ const Header = () => {
       return true;
     }
   };
+
   return (
     <div className='bg-black/50 backdrop-blur-sm w-full border-b shadow-sm fixed z-50'>
       <header className=' flex justify-between items-center px-3 max-w-6xl mx-auto '>
         <div
-          className='cursor-pointer text-white'
+          className='cursor-pointer text-white text-xl flex justify-center items-center'
           onClick={() => navigate('/')}
         >
-          LOGO
+          <HiHome /> HOMELY
         </div>
         <div>
-          <ul className='flex space-x-10'>
+          <ul className='flex justify-center items-center space-x-10'>
             <li
               onClick={() => navigate('/')}
-              className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent ${
+              className={`header_content cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent ${
                 pathMatchRoute('/') && 'text-white/100 border-b-red-500'
               }`}
             >
-              Home
+              Strona główna
             </li>
             <li
               onClick={() => navigate('/offers')}
-              className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent ${
+              className={`header_content cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent ${
                 pathMatchRoute('/offers') && 'text-white/100 border-b-red-500'
               }`}
             >
-              Offers
+              Oferty
             </li>
             <li
               onClick={() => navigate('/profile')}
-              className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/sign-in') ||
-                (pathMatchRoute('/profile') &&
-                  'text-white/100 border-b-red-500')
-              }`}
+              className={`header_content cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent 
+                ${
+                  pathMatchRoute('/sign-in') || pathMatchRoute('/profile')
+                    ? 'text-white/100 border-b-red-500'
+                    : 'border-0'
+                }`}
             >
               {pageState}
+            </li>
+
+            <li className='hamburger py-4' onClick={() => setIsOpen(!isOpen)}>
+              <GiHamburgerMenu />
             </li>
           </ul>
         </div>
       </header>
+      <motion.nav animate={isOpen ? 'open' : 'closed'} variants={variants}>
+        <div className='hamburger bg-black/50 backdrop-blur-sm w-full border-b shadow-sm fixed z-50'>
+          <div
+            onClick={() => {
+              navigate('/');
+              setIsOpen(false);
+            }}
+            className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent flex justify-center ${
+              pathMatchRoute('/') && 'text-white/100 border-b-red-500'
+            }`}
+          >
+            Strona główna
+          </div>
+          <div
+            onClick={() => {
+              navigate('/offers');
+              setIsOpen(false);
+            }}
+            className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent flex justify-center ${
+              pathMatchRoute('/offers') && 'text-white/100 border-b-red-500'
+            }`}
+          >
+            Oferty
+          </div>
+          <div
+            onClick={() => {
+              navigate('/profile');
+              setIsOpen(false);
+            }}
+            className={`cursor-pointer py-4 text-base font-semibold text-white/70 border-b-[3px] border-b-transparent flex justify-center
+                ${
+                  pathMatchRoute('/sign-in') || pathMatchRoute('/profile')
+                    ? 'text-white/100 border-b-red-500'
+                    : 'border-0'
+                }`}
+          >
+            {pageState}
+          </div>
+        </div>
+      </motion.nav>
     </div>
   );
 };
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -15,11 +15,16 @@ import Spinner from '../components/Spinner';
 import ListingItem from '../components/ListingItem';
 
 const Offers = () => {
-  const [listings, setListings] = useState(null);
+  const [listingsOffer, setListingsOffer] = useState(null);
+  const [listingsSale, setListingsSale] = useState(null);
+  const [listingsRent, setListingsRent] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [lastFetchListing, setLastFetchListing] = useState(null);
+  const [lastFetchListingOffer, setLastFetchListingOffer] = useState(null);
+  const [lastFetchListingSale, setLastFetchListingSale] = useState(null);
+  const [lastFetchListingRent, setLastFetchListingRent] = useState(null);
+  const [toggle, setToggle] = useState(false);
   useEffect(() => {
-    const fetchListings = async () => {
+    const fetchListingsOffers = async () => {
       try {
         const listingRef = collection(db, 'listings');
         const q = query(
@@ -30,21 +35,73 @@ const Offers = () => {
         );
         const querySnap = await getDocs(q);
         const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-        setLastFetchListing(lastVisible);
-        const listings = [];
+        setLastFetchListingOffer(lastVisible);
+        const listingsOffer = [];
         querySnap.forEach((doc) => {
-          return listings.push({
+          return listingsOffer.push({
             id: doc.id,
             data: doc.data(),
           });
         });
-        setListings(listings);
+        setListingsOffer(listingsOffer);
         setLoading(false);
       } catch (error) {
         toast.error('Nie można wyświetlić ofert');
       }
     };
-    fetchListings();
+    fetchListingsOffers();
+    const fetchListingsSale = async () => {
+      try {
+        const listingRef = collection(db, 'listings');
+        const q = query(
+          listingRef,
+          where('type', '==', 'sale'),
+          orderBy('timestamp', 'desc'),
+          limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchListingSale(lastVisible);
+        const listingsSale = [];
+        querySnap.forEach((doc) => {
+          return listingsSale.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListingsSale(listingsSale);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Nie można wyświetlić ofert');
+      }
+    };
+    fetchListingsSale();
+    const fetchListingsRent = async () => {
+      try {
+        const listingRef = collection(db, 'listings');
+        const q = query(
+          listingRef,
+          where('type', '==', 'rent'),
+          orderBy('timestamp', 'desc'),
+          limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchListingRent(lastVisible);
+        const listingsRent = [];
+        querySnap.forEach((doc) => {
+          return listingsRent.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListingsRent(listingsRent);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Nie można wyświetlić ofert');
+      }
+    };
+    fetchListingsRent();
   }, []);
 
   const onFetchMoreListings = async () => {
@@ -54,20 +111,72 @@ const Offers = () => {
         listingRef,
         where('offer', '==', true),
         orderBy('timestamp', 'desc'),
-        startAfter(lastFetchListing),
+        startAfter(lastFetchListingOffer),
         limit(4)
       );
       const querySnap = await getDocs(q);
       const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-      setLastFetchListing(lastVisible);
-      const listings = [];
+      setLastFetchListingOffer(lastVisible);
+      const listingsOffer = [];
       querySnap.forEach((doc) => {
-        return listings.push({
+        return listingsOffer.push({
           id: doc.id,
           data: doc.data(),
         });
       });
-      setListings((prevState) => [...prevState, ...listings]);
+      setListingsOffer((prevState) => [...prevState, ...listingsOffer]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Nie można wyświetlić ofert');
+    }
+  };
+  const onFetchMoreListingsSale = async () => {
+    try {
+      const listingRef = collection(db, 'listings');
+      const q = query(
+        listingRef,
+        where('type', '==', 'sale'),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchListingSale),
+        limit(4)
+      );
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchListingSale(lastVisible);
+      const listingsSale = [];
+      querySnap.forEach((doc) => {
+        return listingsSale.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListingsSale((prevState) => [...prevState, ...listingsSale]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Nie można wyświetlić ofert');
+    }
+  };
+  const onFetchMoreListingsRent = async () => {
+    try {
+      const listingRef = collection(db, 'listings');
+      const q = query(
+        listingRef,
+        where('type', '==', 'rent'),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchListingRent),
+        limit(4)
+      );
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchListingRent(lastVisible);
+      const listingsRent = [];
+      querySnap.forEach((doc) => {
+        return listingsRent.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListingsRent((prevState) => [...prevState, ...listingsRent]);
       setLoading(false);
     } catch (error) {
       toast.error('Nie można wyświetlić ofert');
@@ -75,37 +184,111 @@ const Offers = () => {
   };
 
   return (
-    <div className='max-w-6xl mx-auto px-3'>
-      <h1 className='text-3xl text-center pt-20 mb-6 text-white'>Oferty</h1>
-      {loading ? (
-        <Spinner />
-      ) : listings && listings.length > 0 ? (
-        <>
-          <main>
-            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-              {listings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  id={listing.id}
-                  listing={listing.data}
-                />
-              ))}
-            </ul>
-          </main>
-          {lastFetchListing && (
-            <div className='flex justify-center items-center'>
-              <button
-                onClick={onFetchMoreListings}
-                className='bg-blue-600 px-4 py-2 text-white mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out'
-              >
-                Pokaż więcej
-              </button>
-            </div>
-          )}
-        </>
-      ) : (
-        <p>Brak ofert</p>
-      )}
+    <div>
+      <div className='max-w-6xl mx-auto px-3'>
+        {loading ? (
+          <Spinner />
+        ) : listingsOffer && listingsOffer.length > 0 ? (
+          <>
+            <h1 className='text-3xl text-center pt-20 mb-6 text-white'>
+              Przecenione miejsca
+            </h1>
+            <main>
+              <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {listingsOffer.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                ))}
+              </ul>
+            </main>
+            {lastFetchListingOffer && (
+              <div className='flex justify-center items-center'>
+                <button
+                  onClick={onFetchMoreListings}
+                  className='bg-blue-600 px-4 py-2 text-white mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out'
+                >
+                  Pokaż więcej
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <p className='text-white'>Brak ofert</p>
+        )}
+      </div>
+
+      <div className='max-w-6xl mx-auto px-3'>
+        {loading ? (
+          <Spinner />
+        ) : listingsSale && listingsSale.length > 0 ? (
+          <>
+            <h1 className='text-3xl text-center pt-20 mb-6 text-white'>
+              Miejsca na sprzedaż
+            </h1>
+            <main>
+              <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {listingsSale.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                ))}
+              </ul>
+            </main>
+            {lastFetchListingSale && (
+              <div className='flex justify-center items-center'>
+                <button
+                  onClick={onFetchMoreListingsSale}
+                  className='bg-blue-600 px-4 py-2 text-white mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out'
+                >
+                  Pokaż więcej
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <p className='text-white'>Brak ofert</p>
+        )}
+      </div>
+
+      <div className='max-w-6xl mx-auto px-3'>
+        {loading ? (
+          <Spinner />
+        ) : listingsRent && listingsRent.length > 0 ? (
+          <>
+            <h1 className='text-3xl text-center pt-20 mb-6 text-white'>
+              Miejsca do wynajęcia
+            </h1>
+            <main>
+              <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {listingsRent.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                ))}
+              </ul>
+            </main>
+            {lastFetchListingRent && (
+              <div className='flex justify-center items-center'>
+                <button
+                  onClick={onFetchMoreListingsRent}
+                  className='bg-blue-600 px-4 py-2 text-white mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out'
+                >
+                  Pokaż więcej
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <p className='text-white'>Brak ofert</p>
+        )}
+      </div>
     </div>
   );
 };
